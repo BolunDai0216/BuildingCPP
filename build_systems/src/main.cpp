5094a7ea09e52043881fa0936f24a15405217efa
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2023-02-08 14:17:32 -0500

    update cmake tutorial

---
+++ build_systems/src/main.cpp
@@ -0,0 +1,9 @@
+#include "tools.hpp"
+#include <iostream>
+
+int main()
+{
+  MakeItRain();
+  MakeItSunny();
+  return 0;
+}
---
+++ build_systems/src/tools.cpp
@@ -0,0 +1,12 @@
+#include "tools.hpp"
+#include <iostream>
+
+void MakeItSunny()
+{
+  std::cout << "It's now sunny\n";
+}
+
+void MakeItRain()
+{
+  std::cerr << "Not yet implemented\n";
+}
