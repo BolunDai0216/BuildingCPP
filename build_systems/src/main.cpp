573b3dcfc68cf57138623ca23aa954dc2f737040
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2023-02-08 21:10:09 -0500

    updated build_systems

--- build_systems/src/main.cpp
+++ build_systems/src/main.cpp
@@ -1,9 +1,14 @@
 #include "tools.hpp"
 #include <iostream>
+#include <Eigen/Core>
 
 int main()
 {
   MakeItRain();
   MakeItSunny();
+
+  Eigen::MatrixXd mat = Eigen::MatrixXd::Ones(6, 6);
+  std::cout << mat << std::endl;
+
   return 0;
 }
