40adad9e20658d79c5b4410e52b31bc7156a264c
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2024-08-17 12:20:49 -0400

    updated github pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Building CPP"
+project = "Building C++"
 copyright = "2024, Bolun Dai"
 author = "Bolun Dai"
 
@@ -73,7 +73,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_title = "Building CPP"
+html_title = "Building C++"
 myst_enable_extensions = [
     "dollarmath",
     "amsmath",
